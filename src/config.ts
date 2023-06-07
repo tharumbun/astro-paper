4c89e44a1d324950f42330ec1ca5ea63796acc9d
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-07 13:17:26 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -56,13 +56,6 @@ export const SOCIALS: SocialObjects = [
     linkTitle: `${SITE.title} on WhatsApp`,
     active: false,
   },
-
-  {
-    name: "Discord",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Discord`,
-    active: false,
-  },
   {
     name: "Reddit",
     href: "https://github.com/satnaing/astro-paper",
@@ -70,27 +63,9 @@ export const SOCIALS: SocialObjects = [
     active: false,
   },
   {
-    name: "Skype",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Skype`,
-    active: false,
-  },
-  {
-    name: "Steam",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Steam`,
-    active: false,
-  },
-  {
     name: "Telegram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://t.me/tharum",
     linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Mastodon`,
     active: false,
   },
 ];
