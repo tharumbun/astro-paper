2e30b3d81575331319f5eb5dabdf7eba1d0f3b0f
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-07 18:19:23 +0700

    Update _schemas.ts

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -2,12 +2,16 @@ import { z } from "astro:content";
 
 export const blogSchema = z
   .object({
-    pubDate: z.date(),
+    author: z.string().optional(),
+    pubDatetime: z.date(),
     title: z.string(),
+    postSlug: z.string().optional(),
     featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
     tags: z.array(z.string()).default(["others"]),
+    ogImage: z.string().optional(),
     description: z.string(),
   })
-  // .strict();
+  .strict();
 
 export type BlogFrontmatter = z.infer<typeof blogSchema>;
