4ce9f350f8f25febd93d3f8cdde0fdddca23d531
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-07 18:05:39 +0700

    Update _schemas.ts

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -2,14 +2,10 @@ import { z } from "astro:content";
 
 export const blogSchema = z
   .object({
-    author: z.string().optional(),
     pubDate: z.date(),
     title: z.string(),
-    postSlug: z.string().optional(),
     featured: z.boolean().optional(),
-    draft: z.boolean().optional(),
     tags: z.array(z.string()).default(["others"]),
-    ogImage: z.string().optional(),
     description: z.string(),
   })
   .strict();
