451fed7c8d002ebb5a2d036d45364f42c57448d4
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-08 22:47:07 +0700

    Update _schemas.ts

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -5,6 +5,7 @@ export const blogSchema = z
     author: z.string().optional(),
     pubDatetime: z.date().optional(),
     title: z.string(),
+    url: z.string(),
     featured: z.boolean().optional(),
     tags: z.array(z.string()).default(["others"]),
     description: z.string(),
