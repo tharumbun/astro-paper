5a5a7b7b889c3249055966d768f3a18c44b8cc66
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-08 21:20:02 +0700

    Update slugify.ts

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,11 +1,17 @@
 import { slug as slugger } from "github-slugger";
 import type { BlogFrontmatter } from "@content/_schemas";
 
-export const slugifyStr = (str: string) => slugger(str);
+export const getFileNameSlug = (filename: string) => {
+  const extension = filename.split(".").pop();
+  const name = filename.replace(extension, "");
+  return slugger(name);
+};
 
-const slugify = (post: BlogFrontmatter) =>
-  post.postSlug ? slugger(post.postSlug) : slugger(post.title);
+export const slugify = (post: BlogFrontmatter) => {
+  const filename = post.filename;
+  return getFileNameSlug(filename);
+};
 
-export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
+export const slugifyAll = (arr: string[]) => arr.map(str => getFileNameSlug(str));
 
 export default slugify;
