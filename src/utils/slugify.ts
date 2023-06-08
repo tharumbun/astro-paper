c8f50a9613fa2bbdeaf75d148b6e15422ede9de9
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-08 22:23:07 +0700

    Update slugify.ts

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -3,7 +3,8 @@ import type { BlogFrontmatter } from "@content/_schemas";
 
 export const slugifyStr = (str: string) => slugger(str);
 
-const slug = slugger(url);
+const slugify = (post: BlogFrontmatter) =>
+  post.postSlug ? slugger(post.postSlug) : slugger(post.title);
 
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
 
